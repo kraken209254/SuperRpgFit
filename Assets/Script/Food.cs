d749f67e27c980295c249744e9be94bdc816e056
CosmicBlue <kraken209254@gmail.com>
2023-06-02 21:36:01 -0600

    Squashed commit of the following:
    commit f60debd767944b90411623c233b84a067dcb6aec
    Author: KevinA00833985 <105224601+KevinA00833985@users.noreply.github.com>
    Date:   Fri Jun 2 13:10:51 2023 -0600
    
        lol se me olvida escribir
    
    commit a5640ac1c6bbdbe36daea4249be62e59308d7e30
    Author: KevinA00833985 <105224601+KevinA00833985@users.noreply.github.com>
    Date:   Fri Jun 2 13:01:26 2023 -0600
    
        cambio de ingles a español
    
    commit 6a7392ead05e1530b6925c2bb473e21d6b16f141
    Author: KevinA00833985 <105224601+KevinA00833985@users.noreply.github.com>
    Date:   Fri Jun 2 09:41:41 2023 -0600
    
        mejoras en visulacion y tiempo
    
    commit 99a118cbd942d2676e096cf7ad196e0269ee7042
    Author: KevinA00833985 <105224601+KevinA00833985@users.noreply.github.com>
    Date:   Thu Jun 1 17:29:56 2023 -0600
    
        visulacion y modificacion de unas cosas del codigo
    
    commit 1244b825299ecc123128ff2f4c12e32ac74fab5e
    Author: KevinA00833985 <105224601+KevinA00833985@users.noreply.github.com>
    Date:   Wed May 31 14:42:23 2023 -0600
    
        Mejora de thirst
    
    commit 83ad8b89128f388cb0dcb055ed16aeee1e51af97
    Author: KevinA00833985 <105224601+KevinA00833985@users.noreply.github.com>
    Date:   Wed May 31 09:30:10 2023 -0600
    
        alimetacion

---
+++ Assets/Script/Food.cs
@@ -0,0 +1,291 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Food : MonoBehaviour
+{
+    public Slider hungerBar;
+    public Slider thirstBar;
+    public InputField foodInputField;
+    public InputField[] ingredientInputFields;
+    public InputField waterInputField;
+    public Button submitFoodButton;
+    public Button[] submitIngredientButtons;
+    public Button submitWaterButton;
+    public GameObject hungerPopupPanel;
+    public GameObject thirstPopupPanel;
+
+    private const float reductionAmount = 0.30f;
+    private const float ingredientIncreaseAmount = 0.06f;
+    private const float requiredWaterAmount = 500f;
+    private const float maxHungerLevel = 1f;
+    private const float maxThirstLevel = 1f;
+
+    private float hungerLevel = 1f;
+    private float thirstLevel = 1f;
+    private bool isHungerFull = false;
+    private bool isThirstFull = false;
+    private bool hasSubmittedFood = false;
+    private string currentFood = "";
+    private int ingredientCount = 0;
+    private DateTime currentDateTime;
+    private DateTime currentDay;
+
+    [SerializeField]
+    private Text hungerText;
+
+    [SerializeField]
+    private Text thirstText;
+
+    [SerializeField]
+    private Text currentTimeText;
+
+    [SerializeField]
+    private Text mealText;
+
+    private bool hasReducedLevels = false;
+
+    private void Start()
+    {
+        submitFoodButton.onClick.AddListener(SubmitFoodInput);
+
+        for (int i = 0; i < submitIngredientButtons.Length; i++)
+        {
+            int index = i;
+            submitIngredientButtons[index].onClick.AddListener(() => SubmitIngredientInput(index));
+        }
+
+        submitWaterButton.onClick.AddListener(SubmitWaterInput);
+
+        UpdateHungerAndThirstBars(); 
+        UpdateHungerAndThirstText(); 
+
+        UpdateCurrentDay();
+        DisplayMealText();
+    }
+
+    private void Update()
+    {
+        currentDateTime = DateTime.Now;
+
+        if (currentDateTime.Minute == 0)
+        {
+            ReduceLevelsAtSpecifiedTimes();
+        }
+
+        currentTimeText.text = currentDateTime.ToString("HH:mm:ss");
+
+        DisplayMealText(); 
+    }
+
+    private void ReduceLevelsAtSpecifiedTimes()
+    {
+        if (currentDateTime.Hour == 9 && !hasReducedLevels)
+        {
+            ReduceHungerAndThirstLevels();
+            //Debug.Log($"Reduced levels at 09:00. Current time: {currentDateTime.ToString("HH:mm:ss")}");
+        }
+        else if (currentDateTime.Hour == 14 && !hasReducedLevels)
+        {
+            ReduceHungerAndThirstLevels();
+            //Debug.Log($"Reduced levels at 14:00. Current time: {currentDateTime.ToString("HH:mm:ss")}");
+        }
+        else if (currentDateTime.Hour == 20 && !hasReducedLevels)
+        {
+            ReduceHungerAndThirstLevels();
+            //Debug.Log($"Reduced levels at 20:00. Current time: {currentDateTime.ToString("HH:mm:ss")}");
+        }
+    }
+
+    private void ReduceHungerAndThirstLevels()
+    {
+        float reductionAmountPerHour = 1f - reductionAmount;
+
+        hungerLevel *= reductionAmountPerHour;
+        thirstLevel *= reductionAmountPerHour;
+
+        if (hungerLevel <= 0f)
+        {
+            hungerLevel = 0f;
+            DisplayHungerPopup();
+        }
+
+        if (thirstLevel <= 0f)
+        {
+            thirstLevel = 0f;
+            DisplayThirstPopup();
+        }
+
+        UpdateHungerAndThirstBars();
+        UpdateHungerAndThirstText();
+
+        hasReducedLevels = true;
+    }
+
+    private void UpdateCurrentDay()
+    {
+        currentDay = currentDateTime.Date;
+    }
+
+    private void SubmitFoodInput()
+    {
+        string food = foodInputField.text;
+
+        if (!string.IsNullOrEmpty(food))
+        {
+            hasSubmittedFood = true;
+            currentFood = food;
+            ingredientCount = 0;
+            IncreaseHungerLevel();
+            ClearFoodInputField();
+            ClearIngredientInputFields();
+        }
+    }
+
+    private void SubmitIngredientInput(int index)
+    {
+        string ingredient = ingredientInputFields[index].text;
+
+        if (hasSubmittedFood && !string.IsNullOrEmpty(ingredient) && ingredientCount < 4)
+        {
+            IncreaseHungerLevel();
+            ClearIngredientInputField(index);
+            ingredientCount++;
+
+            if (ingredientCount >= 4)
+            {
+                hasSubmittedFood = false;
+                ClearFoodInputField();
+                ClearIngredientInputFields();
+            }
+        }
+    }
+
+    private void SubmitWaterInput()
+    {
+        float waterAmount;
+        if (float.TryParse(waterInputField.text, out waterAmount))
+        {
+            if (waterAmount <= 500f)
+            {
+                float waterIncrease = (waterAmount / requiredWaterAmount) * reductionAmount;
+                thirstLevel += waterIncrease;
+
+                if (thirstLevel >= 1f)
+                {
+                    thirstLevel = 1f;
+                    DisplayThirstPopup();
+                }
+
+                UpdateHungerAndThirstBars();
+                UpdateHungerAndThirstText();
+            }
+            else
+            {
+                // Handle value exceeding the limit (e.g., show an error message)
+            }
+        }
+        else
+        {
+            // Handle invalid input (non-numeric or empty)
+        }
+
+        waterInputField.text = "";
+    }
+
+    private void IncreaseHungerLevel()
+    {
+        float ingredientIncrease = ingredientIncreaseAmount;
+        hungerLevel += ingredientIncrease;
+
+        if (hungerLevel >= 1f)
+        {
+            hungerLevel = 1f;
+            DisplayHungerPopup();
+        }
+
+        UpdateHungerAndThirstBars();
+        UpdateHungerAndThirstText();
+    }
+
+    private void ClearFoodInputField()
+    {
+        foodInputField.text = "";
+    }
+
+    private void ClearIngredientInputFields()
+    {
+        foreach (InputField inputField in ingredientInputFields)
+        {
+            inputField.text = "";
+        }
+    }
+
+    private void ClearIngredientInputField(int index)
+    {
+        ingredientInputFields[index].text = "";
+    }
+
+    private void DisplayHungerPopup()
+    {
+        hungerPopupPanel.SetActive(true);
+        Invoke(nameof(HideHungerPopup), 5f);
+    }
+
+    private void DisplayThirstPopup()
+    {
+        thirstPopupPanel.SetActive(true);
+        Invoke(nameof(HideThirstPopup), 5f);
+    }
+
+    private void HideHungerPopup()
+    {
+        hungerPopupPanel.SetActive(false);
+    }
+
+    private void HideThirstPopup()
+    {
+        thirstPopupPanel.SetActive(false);
+    }
+
+    private void UpdateHungerAndThirstBars()
+    {
+        hungerBar.value = hungerLevel;
+        thirstBar.value = thirstLevel;
+    }
+
+    private void UpdateHungerAndThirstText()
+    {
+        hungerText.text = $"{(int)(hungerLevel * 100)}/{(int)(maxHungerLevel * 100)}";
+        thirstText.text = $"{(int)(thirstLevel * 100)}/{(int)(maxThirstLevel * 100)}";
+    }
+
+    private void DisplayMealText()
+    {
+        TimeSpan currentTime = currentDateTime.TimeOfDay;
+
+        TimeSpan breakfastTime = new TimeSpan(9, 0, 0);
+        TimeSpan lunchTime = new TimeSpan(14, 0, 0);
+        TimeSpan dinnerTime = new TimeSpan(20, 0, 0);
+
+        if (currentTime >= breakfastTime && currentTime < lunchTime)
+        {
+            mealText.text = "Desayuno";
+        }
+        else if (currentTime >= lunchTime && currentTime < dinnerTime)
+        {
+            mealText.text = "Comida";
+        }
+        else if (currentTime >= dinnerTime || currentTime < breakfastTime)
+        {
+            mealText.text = "Cena";
+        }
+        else
+        {
+            mealText.text = "";
+        }
+    }
+
+}
