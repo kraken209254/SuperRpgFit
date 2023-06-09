2a9a0120df5e85a65d015c0fc0ef3eb4a6f72af5
CosmicBlue <kraken209254@gmail.com>
2023-06-08 18:50:47 -0600

    New System Timer

--- Assets/Scripts/CheckForNumber.cs
+++ Assets/Scripts/CheckForNumber.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using TMPro;
+
 public class CheckForNumber : MonoBehaviour
 {
+
     public static int hora1;
     public static int hora2;
     public static int min1;
@@ -13,49 +15,6 @@ public class CheckForNumber : MonoBehaviour
     public static int sec2;
     public GameObject[] TextosHoras;
     public List<RaycastResult> RaycastResults;
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log(collision.gameObject.tag);
-
-        if (collision.gameObject.tag == "Selector")
-        {
-            Debug.Log("Collision");
-            switch (gameObject.transform.parent.tag)
-            {
-                case "Hora1":
-                    hora1 = int.Parse(gameObject.GetComponent<TextMeshProUGUI>().text);
-                    break;
-                case "Hora2":
-                    Debug.Log(hora2);
-                    hora2 = int.Parse(gameObject.GetComponent<TextMeshProUGUI>().text);
-
-                    break;
-                case "Min1":
-                    min1 = int.Parse(gameObject.GetComponent<TextMeshProUGUI>().text);
-
-                    break;
-                case "Min2":
-                    min2 = int.Parse(gameObject.GetComponent<TextMeshProUGUI>().text);
-
-                    Debug.Log(min2);
-                    break;
-                case "Sec1":
-                    sec1 = int.Parse(gameObject.GetComponent<TextMeshProUGUI>().text);
-
-                    Debug.Log(sec1);
-
-                    break;
-                case "Sec2":
-                    sec2 = int.Parse(gameObject.GetComponent<TextMeshProUGUI>().text);
-
-                    Debug.Log(sec2);
-
-                    break;
-
-            }
-        }
-    }
 
     public void Update()
     {
@@ -69,4 +28,35 @@ public class CheckForNumber : MonoBehaviour
             TextosHoras[6].GetComponent<TextMeshProUGUI>().text = sec2.ToString();
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Selector")
+        {
+            switch (gameObject.transform.parent.tag)
+            {
+                case "Hora1":
+                    hora1 = int.Parse(gameObject.GetComponent<TextMeshProUGUI>().text);
+                    break;
+                case "Hora2":
+                    Debug.Log(hora2);
+                    hora2 = int.Parse(gameObject.GetComponent<TextMeshProUGUI>().text);
+                    break;
+                case "Min1":
+                    min1 = int.Parse(gameObject.GetComponent<TextMeshProUGUI>().text);
+                    break;
+                case "Min2":
+                    min2 = int.Parse(gameObject.GetComponent<TextMeshProUGUI>().text);
+                    break;
+                case "Sec1":
+                    sec1 = int.Parse(gameObject.GetComponent<TextMeshProUGUI>().text);
+                    break;
+                case "Sec2":
+                    sec2 = int.Parse(gameObject.GetComponent<TextMeshProUGUI>().text);
+                    break;
+            }
+        }
+    }
+
+
 }
